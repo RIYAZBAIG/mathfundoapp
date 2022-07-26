fd79420e3e05f4bdab0a84d528724a12f06b8677
pickupbiz <43946129+pickupbiz@users.noreply.github.com>
2022-07-26 15:50:18 +0530

    Table& Square

--- src/components/Tabels.jsx
+++ src/components/Tabels.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Card, CardContent, Typography } from "@mui/material";
+import { Table2 } from "./Tables/Tables2";
+import { DynTable } from "./Tables/DynTable";
 
 export const Tables = () => {
   return (
     <Card sx={{ bgcolor: "coral" }}>
       <CardContent>
         <Typography variant="h4">Tables</Typography>
+        <DynTable />
       </CardContent>
     </Card>
   );
